3d54acc79a83e75eb1adbb55c3fc9c4424142b69
Hieu Minh Le <hieucoder@outlook.com>
2021-03-08 09:19:24 +0700

    Initial commit.

---
+++ src/dev/tech/Main.java
@@ -0,0 +1,27 @@
+package dev.tech;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        UserDetails userDetails = new UserDetails();
+        userDetails.setUserId(1);
+        userDetails.setUsername("hieu");
+        userDetails.setJoinedDate(new Date());
+        userDetails.setAddress("Viet Nam");
+        userDetails.setDescription("Java Engineer");
+
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(userDetails);
+        session.getTransaction().commit();
+        session.close();
+
+        userDetails = null;
+    }
+}
---
+++ src/dev/tech/UserDetails.java
@@ -0,0 +1,58 @@
+package dev.tech;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity/*(name = "USER_DETAILS")*/
+@Table(name = "USER_DETAILS")
+public class UserDetails {
+    @Id
+    private int userId;
+//    @Transient // Telling Hibernate not to persist this field in the database.
+    private String username;
+    @Temporal(TemporalType.DATE)
+    private Date joinedDate;
+    private String address;
+    @Lob
+    private String description;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getJoinedDate() {
+        return joinedDate;
+    }
+
+    public void setJoinedDate(Date joinedDate) {
+        this.joinedDate = joinedDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
